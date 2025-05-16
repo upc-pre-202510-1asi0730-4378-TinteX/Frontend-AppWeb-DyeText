228e73a9af22fe14e0a4088879dc4b7aef7835d4
Gabriel Gordon <116988979+Silent343@users.noreply.github.com>
2025-05-15 21:25:46 -0500

    feat(monitoring): add machines to api

---
+++ src/monitoring/component/textilMachine-item-search.component.vue
@@ -0,0 +1,118 @@
+<script>
+import DataManager from "../../shared/components/data-manager.component.vue";
+
+export default {
+  name: "textilMachine-search-content",
+  components: {DataManager}
+}
+</script>
+
+<template>
+  <div class="filter-search-container">
+    <div class="filter-box">
+      <div class="filter">
+        <h3>Estado</h3>
+        <div class="filter-type">
+          <ul>
+            <li class="active">Active</li>
+            <li>Maintenance</li>
+            <li>Stopped</li>
+          </ul>
+        </div>
+      </div>
+      <div class="filter">
+        <h3>Asset Type</h3>
+        <div class="filter-type">
+          <ul>
+            <li>Type A</li>
+            <li>Type B</li>
+            <li>Type C</li>
+          </ul>
+        </div>
+      </div>
+        <div class="filter">
+          <h3>Zone</h3>
+          <div class="filter-type">
+            <ul>
+              <li>Zone 1</li>
+              <li>Zone 2</li>
+              <li>Zone 3</li>
+            </ul>
+          </div>
+        </div>
+    </div>
+    <div class="search-box">
+      <form>
+          <div class="search-input-container">
+            <input class="search" placeholder="search device">
+            <i class="pi pi-search"></i>
+          </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .filter-search-container{
+    display: flex;
+    flex-direction: row;
+    padding: 20px;
+    justify-content: space-around;
+    width: 100%;
+  }
+  .filter-box{
+    width: 30rem;
+  }
+  .filter{
+    margin: 20px 0;
+    text-align: start;
+    color: #003366;
+    ul{
+      list-style: none;
+      display: flex;
+      flex-direction: row;
+      padding-top: 10px;
+
+      li{
+        background-color: #66b2ff;
+        padding: 5px 10px;
+        border-radius: 5px;
+        margin: 0 10px;
+      }
+    }
+  }
+  .search-box{
+    width: 40rem;
+    padding: 10px;
+    display: flex;
+    justify-content: end;
+  }
+  .search-input-container{
+    border: 1px solid #003366;
+    border-radius: 5px;
+    width: 30rem;
+
+
+    input{
+      width: 80%;
+      background-color: transparent;
+      outline: none;
+      border: none;
+      padding: 10px;
+      color: #000000;
+      &:focus{
+        border: none;
+      }
+    }
+
+    i{
+      color: #000000;
+    }
+  }
+
+
+  .active{
+    background-color: #003366!important;
+    color: #fff;
+  }
+</style>
---
+++ src/monitoring/pages/textilMachine-management.component.vue
@@ -0,0 +1,154 @@
+<script>
+import TextilMachineSearchContent from "../component/textilMachine-item-search.component.vue";
+import TextilMachineCreateComponent from "../component/textilMachine-item-create.component.vue";
+import {TextileMachine} from "../model/textileMachine.entity.js";
+import {TextileMachineService} from "../services/textilMachine.service.js";
+import DataManager from "../../../../frontend_dyetex/src/shared/components/data-manager.component.vue";
+
+export default {
+  name: "textilMachine-management",
+  components: {TextilMachineCreateComponent, TextilMachineSearchContent, DataManager},
+
+  /**
+   * @returns {Object} Component data
+   */
+  data() {
+    return {
+      title: {singular: "Machine", plural: "Machines"},
+      machines: [],
+      machine: new TextileMachine({}),
+      selectedMachines: [],
+      machineService: null,
+      createAndEditDialogIsVisible: false,
+      submitted: false,
+      isEdit: false,
+    }
+  },
+  methods:{
+    notifySuccessfulAction(message) {
+      this.$toast.add({severity: 'success', summary: 'Success', detail: message, life: 3000});
+    },
+    findIndexById(id) {
+      return this.machines.findIndex(machine => machine.id === id);
+    },
+    onNewItem() {
+      this.machine = new TextileMachine({});
+      this.isEdit = false;
+      this.submitted = false;
+      this.createAndEditDialogIsVisible = true;
+      console.log(this.createAndEditDialogIsVisible);
+    },
+    onCancelRequested() {
+      this.createAndEditDialogIsVisible = false;
+      this.submitted = false;
+      this.isEdit = false;
+    },
+    onSaveRequested(item) {
+      console.log('onSaveRequested');
+      this.submitted = true;
+      if (item.name.trim()) {
+        if (item.id) {
+          this.updateMachine();
+        } else {
+          this.createMachine();
+        }
+        this.createAndEditDialogIsVisible = false;
+        this.isEdit = false;
+      }
+    },
+
+
+    createMachine() {
+      const statuses = ['Active', 'Maintenance', 'Stopped'];
+      this.machine.status = statuses[Math.floor(Math.random() * statuses.length)];
+      this.machine.dateInstalation = new Date().toISOString().split('T')[0];
+
+      this.machineService.create(this.machine).then(response => {
+        let created  = new TextileMachine(response.data);
+        this.machines.push(created);
+        this.notifySuccessfulAction("Machine Created");
+      }).catch(error => console.error(error));
+    },
+
+    /**
+     * Updates an existing category via the API
+     * Updates the local category in the array on success
+     */
+    updateMachine() {
+      this.machineService.update(this.machine.id, this.machine).then(response => {
+        console.log('updateMachine');
+        let index = this.findIndexById(this.machine.id);
+        this.machines[index] = new TextileMachine(response.data);
+        console.log(this.machines);
+        this.notifySuccessfulAction("Machine Updated");
+      }).catch(error => console.error(error));
+    },
+
+
+
+    onDeleteItem(item) {
+      this.machine = {...item};
+      this.deleteMachine();
+    },
+
+    onDeleteSelectedItems(selectedItems) {
+      this.selectedMachines = selectedItems;
+      this.deleteSelectedMachines();
+    },
+
+    deleteMachine() {
+      this.machineService.delete(this.machine.id).then(() => {
+        let index = this.findIndexById(this.machine.id);
+        this.machines.splice(index, 1);
+        this.notifySuccessfulAction("Category Deleted");
+      }).catch(error => console.error(error));
+    },
+
+    deleteSelectedMachines() {
+      this.deleteSelectedMachines.forEach((machine) => {
+        this.machineService.delete(machine.id).then(() => {
+          this.machines = this.machines.filter((t) => t.id !== this.machine.id);
+        });
+      });
+      this.notifySuccessfulAction("Categories Deleted");
+    }
+  },
+  created() {
+    this.machineService = new TextileMachineService();
+    this.machineService.getAll().then(response => {
+      this.machines = response.data.map(item => new TextileMachine(item));
+      console.log(this.machines);
+    }).catch(error => console.error(error));
+  },
+}
+</script>
+
+<template>
+  <textil-machine-search-content></textil-machine-search-content>
+  <div class="w-full">
+    <data-manager :title=title
+                  :items="machines"
+                  @new-item-requested="onNewItem"
+                  @delete-item-requested="onDeleteItem($event)"
+                  @delete-selected-items-requested="onDeleteSelectedItems($event)">
+      <template #custom-columns>
+        <pv-column :sortable="true" field="id" header="Id" style="min-width: 12rem" />
+        <pv-column :sortable="true" field="name" header="Name" style="min-width: 12rem"/>
+        <pv-column :sortable="true" field="numberMachine" header="Number Machine" style="min-width: 12rem" />
+        <pv-column :sortable="true" field="zone" header="Zone" style="min-width: 12rem"/>
+        <pv-column :sortable="true" field="status" header="Status"  style="min-width: 12rem" />
+        <pv-column :sortable="true" field="dateInstalation" header="Installation Date"  style="min-width: 12rem"/>
+      </template>
+    </data-manager>
+    <textil-machine-create-component
+        :edit="isEdit"
+        :item="machine"
+        :visible="createAndEditDialogIsVisible"
+        @cancel-requested="onCancelRequested"
+        @save-requested="onSaveRequested($event)"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
